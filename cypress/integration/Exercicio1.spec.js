fb1b210f413e42bb83eec2fecf790d5b7410b9fb
CRISTIANO DURO MARQUES <cristiano@90ti.local>
2022-11-18 15:30:07 -0300

    Exercicio 5 feito

--- cypress/integration/Exercicio1.spec.js
+++ cypress/integration/Exercicio1.spec.js
@@ -1,12 +1,14 @@
 /// <reference types="Cypress" />
 
+
+
 describe('Central de Atendimento ao Cliente TAT', function() {
     beforeEach(() => {
         cy.visit('/src/index.html')
     });
     
     
-    it('verifica o título da aplicação', function() {
+    it('Preencher enviar e verificar se está aparecendo mensagem', function() {
         const longText = 'Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, '
         cy.get('#firstName').type('Cristiano')
         cy.get('#lastName').type('Marques')
@@ -32,7 +34,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             
         });
 
-        it.only('Exibe mensagem de erro quando o telefone se torna obrigatório mas nã é preenchido antes do envio do formulario', function()  {
+        it('Exibe mensagem de erro quando o telefone se torna obrigatório mas nã é preenchido antes do envio do formulario', function()  {
             cy.get('#firstName').type('Cristiano')
             cy.get('#lastName').type('Marques')
             cy.get('#email').type('Cristiano.duro97@gmail.com')
@@ -41,4 +43,29 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('.button').click()
             cy.get('.error > strong').should('be.visible')
         });
+        it.only('Preenche e limpa os campos nome, sobrenome, emmail. e telefone', function()  {
+            cy.get('#firstName')
+            .type('Cristiano')
+            .should('have.value', 'Cristiano')
+            .clear()
+            .should('have.value', '')
+            cy.get('#lastName')
+            .type('Marques')
+            .should('have.value', 'Marques')
+            .clear()
+            .should('have.value', '')
+            cy.get('#email')
+            .type('Cristiano.duro97@gmail.com')
+            .should('have.value', 'Cristiano.duro97@gmail.com')
+            .clear()
+            .should('have.value', '')
+            cy.get('#open-text-area')
+            .type('Teste')
+            .should('have.value', 'Teste')
+            .clear()
+            .should('have.value', '')
+
+        });
+
+
     })
