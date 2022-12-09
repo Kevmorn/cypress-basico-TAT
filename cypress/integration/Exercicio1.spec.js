b3b020c7fd09ba24bf85c505145b642c8edad3e9
Cristiano Duro Marques <cristiano.duro97@gmail.com>
2022-12-09 00:04:58 -0300

    Problemas

--- cypress/integration/Exercicio1.spec.js
+++ cypress/integration/Exercicio1.spec.js
@@ -43,7 +43,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('.button').click()
             cy.get('.error > strong').should('be.visible')
         });
-        it.only('Preenche e limpa os campos nome, sobrenome, emmail. e telefone', function()  {
+        it('Preenche e limpa os campos nome, sobrenome, emmail. e telefone', function()  {
             cy.get('#firstName')
             .type('Cristiano')
             .should('have.value', 'Cristiano')
@@ -65,10 +65,47 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .clear()
             .should('have.value', '')
         });
-        it.only('Envia o Relatorio com sucesso  usando comando costumizado', function() {
+        it('Envia o Relatorio com sucesso  usando comando costumizado', function() {
             cy.filMandatoryFieldsAndSubmit()
             cy.get('.success > strong')
             .should('be.visible')
         });
 
-        });
+        it('Selecione um produto (youtube) por seu texto',  function() {
+            cy.get('#product').select('YouTube')
+            .should('have.value', 'YouTube')
+        });
+        it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+            cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+        });
+
+        it('Seleciona um produto (Blog) por seu índice', function() {
+            cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+        });
+
+        it('marca o tipo de atendimento "Feedback" ',  function() {
+            cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+        });
+        it('Marca cada tipo de atendimento', function() {
+            cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function($radio){
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            }) 
+
+            it.only('Marca ambos checkboxes, depois desmarca o último', function() {
+                cy.get('input[types="checkbox"]')
+                .check()
+                .last()
+                .uncheck()
+                .should('not.be.checked')
+            });
+        });
+});
